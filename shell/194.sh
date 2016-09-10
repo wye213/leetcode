4d356d564a0c3550919a42c79e438fd96f003875
wye213 <wye213@gmail.com>
2016-09-10 14:56:51 +0800

    add shell code

---
+++ shell/194.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FILE=file.txt
+
+awk '{for(i=0;++i<=NF;)a[i]=a[i]?a[i] FS $i:$i}END{for(i=0;i++<NF;)print a[i]}' $FILE 
---
+++ shell/195.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FILE=file.txt
+
+sed -n '10p' $FILE 
